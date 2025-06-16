ea42f18a6236ba126e74b6c8b23d86c0386cc587
ninazhang2025 <roseberry-@hotmail.com>
2025-06-16 16:20:56 +0200

    Update Emission_Per_Year_Per_Sector.R

--- Emission_Per_Year_Per_Sector.R
+++ Emission_Per_Year_Per_Sector.R
@@ -1,7 +1,7 @@
 library(tidyr)
 library(ggplot2)
 
-# Make a double bar chart for the years 2019-2023 for Landbouw and Bouw with the cleaned data set of Emission (DataSetEmissieClean) and use a scale log10 on the y-axis because the Emission values of Landbouw are significantly higher than Bouw.
+#Make a double bar chart for the years 2019-2023 for Landbouw and Bouw with the cleaned data set of Emission (DataSetEmissieClean) and use a scale log10 on the y-axis because the Emission values of Landbouw are significantly higher than Bouw.
 
 ggplot(DataSetEmissieClean, aes(x = factor(Jaar), y = Emissie, fill = Sector)) +
   scale_y_log10() +
