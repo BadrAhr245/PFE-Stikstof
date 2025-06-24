0dcfa941ed786d41543a73d6b9739751ce88a903
noureddineek <noureddine.ek@hotmail.com>
2025-06-24 14:44:41 +0200

    Update Event_Analysis.R

--- Event_Analysis.R
+++ Event_Analysis.R
@@ -1,21 +1,29 @@
-library(readxl)
-library(tidyverse)
+# Load the required libraries
+library(readxl) # For reading excel files
+library(tidyverse) # For data manipulation and visualizations
 
+# Read the first row of the excel file to extract the column names
 kolomnamen <- read_excel("EnergieverbruikLandbouw.xlsx", skip = 0, n_max = 1) %>%
   unlist() %>%
   as.character()
 
+# Read the actual data from the 4th row
 Perioden_Energiegebruik <- read_excel("EnergieverbruikLandbouw.xlsx", skip = 3, col_names = FALSE)
 
+# Assigning the previous extracted column names to the dataset
 colnames(Perioden_Energiegebruik) <- kolomnamen
 
+# Filter the data to only include the row called "Totaal energiedragers"
+#Select only the columns for the years 2019 to 2023
 Perioden_EnergiegebruikClean <- Perioden_Energiegebruik %>%
   filter(Perioden == "Totaal energiedragers") %>%
   select(Perioden, `2019`, `2020`, `2021`, `2022`, `2023**`)
 
+# Rename the column 2023** to 2023
 Perioden_EnergiegebruikClean <- Perioden_EnergiegebruikClean %>%
   rename(`2023` = `2023**`)
 
+# Reshape the data from wide to long format
 Event_analysis_plot <- Perioden_EnergiegebruikClean %>%
 pivot_longer(
 cols = c("2019", "2020", "2021", "2022", "2023"),
@@ -26,7 +34,7 @@ mutate(jaar = as.numeric(jaar),
     energie = as.numeric(energie)
     )
           
-
+# Creating the event analysis plot
 ggplot(Event_analysis_plot, aes(x = jaar, y = energie)) +
 geom_line(color = "steelblue", size = 1.2) +
 geom_point(size = 2) +
